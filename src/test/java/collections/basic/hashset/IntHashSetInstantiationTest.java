2df27af8e78ed37b0e799d5c3e6fc03728fb2d92
Jitendra Kumar <jkumar@altir.co>
2024-08-03 22:36:38 +0530

    Add more unit tests for IntHashSet instantiation

--- src/test/java/collections/basic/hashset/IntHashSetInstantiationTest.java
+++ src/test/java/collections/basic/hashset/IntHashSetInstantiationTest.java
@@ -11,28 +11,6 @@ class IntHashSetInstantiationTest {
   void instantiation_noArg_succeeds() {
     IntHashSet intHashSet = new IntHashSet();
     assertThat(intHashSet.size()).isZero();
-    assertThat(intHashSet.isEmpty()).isTrue();
-  }
-
-  @Test
-  void instantiation_initialCapacityMoreThanOne_succeeds() {
-    IntHashSet intHashSet = new IntHashSet(33);
-    assertThat(intHashSet.size()).isZero();
-    assertThat(intHashSet.isEmpty()).isTrue();
-  }
-
-  @Test
-  void instantiation_initialCapacityOne_succeeds() {
-    IntHashSet intHashSet = new IntHashSet(1);
-    assertThat(intHashSet.size()).isZero();
-    assertThat(intHashSet.isEmpty()).isTrue();
-  }
-
-  @Test
-  void instantiation_initialCapacityZero_fails() {
-    assertThatThrownBy(() -> new IntHashSet(0))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("initialCapacity must not be less than 1");
   }
 
   @Test
@@ -43,10 +21,86 @@ class IntHashSetInstantiationTest {
   }
 
   @Test
+  void instantiation_initialCapacityZero_fails() {
+    assertThatThrownBy(() -> new IntHashSet(0))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("initialCapacity must not be less than 1");
+  }
+
+  @Test
+  void instantiation_initialCapacityOne_succeeds() {
+    IntHashSet intHashSet = new IntHashSet(1);
+    assertThat(intHashSet.size()).isZero();
+  }
+
+  @Test
+  void instantiation_initialCapacityOne_createdSetIsUsable() {
+    IntHashSet intHashSet = new IntHashSet(1);
+    intHashSet.add(87);
+
+    assertThat(intHashSet.contains(87)).isTrue();
+    assertThat(intHashSet.size()).isOne();
+  }
+
+  @Test
+  void instantiation_initialCapacityOne_createdSetCanAccommodateAnyNumberOfElements() {
+    IntHashSet intHashSet = new IntHashSet(1);
+    for (int e = -4; e < 4; e++) {
+      intHashSet.add(e);
+    }
+
+    for (int e = -4; e < 4; e++) {
+      assertThat(intHashSet.contains(e)).isTrue();
+    }
+    assertThat(intHashSet.size()).isEqualTo(8);
+  }
+
+  @Test
+  void instantiation_initialCapacityMoreThanOne_succeeds() {
+    IntHashSet intHashSet = new IntHashSet(33);
+    assertThat(intHashSet.size()).isZero();
+  }
+
+  @Test
+  void instantiation_initialCapacityMoreThanOne_createdSetIsUsable() {
+    IntHashSet intHashSet = new IntHashSet(41);
+    intHashSet.add(34);
+
+    assertThat(intHashSet.contains(34)).isTrue();
+    assertThat(intHashSet.size()).isOne();
+  }
+
+  @Test
+  void instantiation_initialCapacityMoreThanOne_createdSetCanAccommodateAnyNumberOfElements() {
+    IntHashSet intHashSet = new IntHashSet(41);
+    for (int e = -40; e < 40; e++) {
+      intHashSet.add(e);
+    }
+
+    for (int e = -40; e < 40; e++) {
+      assertThat(intHashSet.contains(e)).isTrue();
+    }
+    assertThat(intHashSet.size()).isEqualTo(80);
+  }
+
+  @Test
   void instantiation_usingEmptyIntHashSet_createsEmptySet() {
     IntHashSet source = new IntHashSet();
     IntHashSet intHashSet = new IntHashSet(source);
     assertThat(intHashSet.size()).isZero();
+  }
+
+  @Test
+  void instantiation_usingEmptyIntHashSet_createdSetCanAccommodateMoreElements() {
+    IntHashSet source = new IntHashSet();
+    IntHashSet intHashSet = new IntHashSet(source);
+    assertThat(intHashSet.size()).isZero();
+
+    intHashSet.add(2);
+    intHashSet.add(3);
+    assertThat(intHashSet.contains(2)).isTrue();
+    assertThat(intHashSet.contains(3)).isTrue();
+    assertThat(intHashSet.size()).isEqualTo(2);
   }
 
   @Test
@@ -56,6 +110,21 @@ class IntHashSetInstantiationTest {
     IntHashSet intHashSet = new IntHashSet(source);
     assertThat(intHashSet.size()).isOne();
     assertThat(intHashSet.contains(167)).isTrue();
+  }
+
+  @Test
+  void instantiation_usingSingletonIntHashSet_createdSetCanAccommodateMoreElements() {
+    IntHashSet source = new IntHashSet();
+    source.add(167);
+    IntHashSet intHashSet = new IntHashSet(source);
+    assertThat(intHashSet.size()).isOne();
+    assertThat(intHashSet.contains(167)).isTrue();
+
+    intHashSet.add(2);
+    intHashSet.add(3);
+    assertThat(intHashSet.contains(2)).isTrue();
+    assertThat(intHashSet.contains(3)).isTrue();
+    assertThat(intHashSet.size()).isEqualTo(3);
   }
 
   @Test
@@ -75,5 +144,30 @@ class IntHashSetInstantiationTest {
     for (int e : expectedElements) {
       assertThat(intHashSet.contains(e)).isTrue();
     }
+  }
+
+  @Test
+  void instantiation_usingNElementIntHashSet_createdSetCanAccommodateMoreElements() {
+    IntHashSet source = new IntHashSet();
+    int[] expectedElements = new int[150];
+    int idx = 0;
+    for (int e = -75; e < 75; e++) {
+      source.add(e);
+      expectedElements[idx++] = e;
+    }
+
+    IntHashSet intHashSet = new IntHashSet(source);
+
+    assertThat(intHashSet.size()).isEqualTo(150);
+
+    for (int e : expectedElements) {
+      assertThat(intHashSet.contains(e)).isTrue();
+    }
+
+    intHashSet.add(1002);
+    intHashSet.add(1003);
+    assertThat(intHashSet.contains(1002)).isTrue();
+    assertThat(intHashSet.contains(1003)).isTrue();
+    assertThat(intHashSet.size()).isEqualTo(152);
   }
 }
